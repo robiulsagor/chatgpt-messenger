4460ebbd4e2eae732ca7d31218449e0029de0830
robiulsagor <robiul100.me@gmail.com>
2023-10-13 23:53:34 +0600

    Homepage design

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,55 @@
+import { BiSun } from "react-icons/bi"
+import { HiOutlineBolt } from "react-icons/hi2"
+import { IoWarningOutline } from "react-icons/io5"
+
 
 export default function Home() {
 
   return (
-    <h2>Hello there</h2>
+    <div className="flex flex-col items-center justify-center h-screen text-white">
+      <h1 className="text-5xl font-bold mb-20">MyGPT</h1>
 
+      <div className="flex space-x-3 text-center">
+
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5 gap-1">
+            <BiSun className="h-8 w-8" />
+            <h2>Examples</h2>
+          </div>
+
+          <div className="space-y-2">
+            <p className="infoText">"Explain Something to me"</p>
+            <p className="infoText">"What is the difference between a dog and a cat?"</p>
+            <p className="infoText">"What is the color of the sun?"</p>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5 gap-1">
+            <HiOutlineBolt className="h-8 w-8" />
+            <h2>Capabilities</h2>
+          </div>
+
+          <div className="space-y-2">
+            <p className="infoText">Change the MyGPT model to use</p>
+            <p className="infoText">Messages are stored in Firebase's Firestore</p>
+            <p className="infoText">How toast notifications when MyGPT is thinking</p>
+          </div>
+        </div>
+
+        <div>
+          <div className="flex flex-col items-center justify-center mb-5 gap-1">
+            <IoWarningOutline className="h-8 w-8" />
+            <h2>Limitations</h2>
+          </div>
+
+          <div className="space-y-2">
+            <p className="infoText">May occasionally generate incorrect information</p>
+            <p className="infoText">May occasionally produce harmful instructions or biased content</p>
+            <p className="infoText">Limited knowledge of world and events after 2021</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
