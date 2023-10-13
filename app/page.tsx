2d6143312c979ca40ecb22e570b26c6978843358
robiulsagor <robiul100.me@gmail.com>
2023-10-13 23:08:53 +0600

    I'm changing site design

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,8 @@
-import Image from 'next/image'
-import Header from './components/Header'
 
 export default function Home() {
+
   return (
-
-    <div className='flex items-center flex-col justify-between  h-screen text-white'>
-      <Header />
-
-
-      <div>
-        {/* bottom part */}
-        <h2>hello</h2>
-      </div>
-    </div>
+    <h2>Hello there</h2>
 
   )
 }
